397d78b820fde8fb2ae34c2bb4aa88cb6d3d162a
unknown <kevin.m.bedi@gmail.com>
2019-08-03 22:46:40 -0700

    Remove debug file

--- src/setMoney.lua
+++
@@ -1,6 +0,0 @@
--- local setMoney = function()
---     SetPlayerState(Player(0), PLAYER_STATE_RESOURCE_GOLD, 15000)
---     print("Test setMoney New")
--- end
-
--- TimerStart(CreateTimer(), 0.0, false, setMoney)
