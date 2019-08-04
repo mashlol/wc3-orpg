8b01a025b706f2d91619640255f970c533c3d782
unknown <kevin.m.bedi@gmail.com>
2019-08-03 19:32:24 -0700

    spell map

--- src/spell.lua
+++ src/spell.lua
@@ -1,21 +1,23 @@
 local fireball = require('src/spells/fireball.lua')
 local dummy = require('src/spells/dummy.lua')
 
+-- TODO base on hero or let people customize or w/e
+local SPELL_MAP = {
+    [1] = fireball,
+    [2] = dummy,
+}
+
 local castSpell = function(playerId, idx)
-    if idx == 1 then
-        return fireball.cast(playerId)
-    end
-    if idx == 2 then
-        dummy.cast(playerId)
+    local spell = SPELL_MAP[idx]
+    if spell ~= nil then
+        spell.cast(playerId)
     end
 end
 
 local getCooldown = function(playerId, idx)
-    if idx == 1 then
-        return fireball.getCooldown(playerId)
-    end
-    if idx == 2 then
-        return dummy.getCooldown(playerId)
+    local spell = SPELL_MAP[idx]
+    if spell ~= nil then
+        return spell.getCooldown(playerId)
     end
     return 0
 end
