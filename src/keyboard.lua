f92233b22dda0a39c9d6dd9ed521e7912b98a90c
unknown <kevin.m.bedi@gmail.com>
2019-08-03 22:44:49 -0700

    Lock camera on spacebar held

--- src/keyboard.lua
+++ src/keyboard.lua
@@ -1,4 +1,5 @@
 local spell = require("src/spell.lua")
+local hero = require("src/hero.lua")
 
 -- TODO let people customize keybindings
 local KEY_MAPPING = {
@@ -13,6 +14,19 @@ local keyPressed = function()
     spell.castSpell(
         GetPlayerId(GetTriggerPlayer()),
         KEY_MAPPING[BlzGetTriggerPlayerKey()])
+end
+
+local spaceDown = function()
+    SetCameraTargetControllerNoZForPlayer(
+        GetTriggerPlayer(),
+        hero.getHero(GetPlayerId(GetTriggerPlayer())),
+        0,
+        0,
+        false)
+end
+
+local spaceUp = function()
+    ResetToGameCameraForPlayer(GetTriggerPlayer(), 0)
 end
 
 local init = function()
@@ -31,11 +45,28 @@ local init = function()
     BlzTriggerRegisterPlayerKeyEvent(trigger, Player(0), OSKEY_R, 0, true)
     BlzTriggerRegisterPlayerKeyEvent(trigger, Player(1), OSKEY_R, 0, true)
 
-
     BlzTriggerRegisterPlayerKeyEvent(trigger, Player(0), OSKEY_F, 0, true)
     BlzTriggerRegisterPlayerKeyEvent(trigger, Player(1), OSKEY_F, 0, true)
 
     TriggerAddAction(trigger, keyPressed)
+
+    local spaceDownTrigger = CreateTrigger()
+
+    BlzTriggerRegisterPlayerKeyEvent(
+        spaceDownTrigger, Player(0), OSKEY_SPACE, 0, true)
+    BlzTriggerRegisterPlayerKeyEvent(
+        spaceDownTrigger, Player(1), OSKEY_SPACE, 0, true)
+
+    TriggerAddAction(spaceDownTrigger, spaceDown)
+
+    local spaceUpTrigger = CreateTrigger()
+
+    BlzTriggerRegisterPlayerKeyEvent(
+        spaceUpTrigger, Player(0), OSKEY_SPACE, 0, false)
+    BlzTriggerRegisterPlayerKeyEvent(
+        spaceUpTrigger, Player(1), OSKEY_SPACE, 0, false)
+
+    TriggerAddAction(spaceUpTrigger, spaceUp)
 end
 
 return {
