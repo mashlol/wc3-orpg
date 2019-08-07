feea294f896c77c1749c0543f0b35ba3d25835a9
unknown <kevin.m.bedi@gmail.com>
2019-08-06 19:51:50 -0700

    Remove targeting init code

--- src/target.lua
+++ src/target.lua
@@ -2,28 +2,6 @@ local hero = require('src/hero.lua')
 
 targets = {}
 
-local onUnitSelected = function()
-    local playerId = GetPlayerId(GetTriggerPlayer())
-    local targetedUnit = GetTriggerUnit()
-    if targetedUnit ~= hero.getHero(playerId) then
-        targets[playerId] = targetedUnit
-    end
-end
-
-local onUnitDeselected = function()
-    targets[GetPlayerId(GetTriggerPlayer())] = nil
-end
-
-local init = function()
-    local deselectTrigger = CreateTrigger()
-    TriggerRegisterAnyUnitEventBJ(deselectTrigger, EVENT_PLAYER_UNIT_DESELECTED)
-    TriggerAddAction(selectTrigger, onUnitDeselected)
-
-    local selectTrigger = CreateTrigger()
-    TriggerRegisterAnyUnitEventBJ(selectTrigger, EVENT_PLAYER_UNIT_SELECTED)
-    TriggerAddAction(selectTrigger, onUnitSelected)
-end
-
 local getTarget = function(playerId)
     return targets[playerId]
 end
@@ -52,7 +30,6 @@ local setTarget = function(playerId, unit)
 end
 
 return {
-    init = init,
     getTarget = getTarget,
     setTarget = setTarget,
 }
