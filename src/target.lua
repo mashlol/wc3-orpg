b4685f35fa1d90336cca371a9fc05ba4a0133abe
unknown <kevin.m.bedi@gmail.com>
2019-08-06 19:51:11 -0700

    Play a sound when target changes

--- src/target.lua
+++ src/target.lua
@@ -33,6 +33,21 @@ local hasTarget = function(playerId)
 end
 
 local setTarget = function(playerId, unit)
+    if targets[playerId] ~= unit then
+        local sound = CreateSound(
+            "Sound\\Interface\\SubGroupSelectionChange1.wav",
+            false,
+            false,
+            false,
+            0,
+            0,
+            "DefaultEAXON")
+        if playerId == GetPlayerId(GetLocalPlayer()) then
+            StartSound(sound)
+        end
+        KillSoundWhenDone(sound)
+    end
+
     targets[playerId] = unit
 end
 
