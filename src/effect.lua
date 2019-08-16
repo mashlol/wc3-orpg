2559fd7ae2132997eaa043ac411ae44c274649d6
unknown <kevin.m.bedi@gmail.com>
2019-08-16 00:03:33 -0700

    Optimize effects code

--- src/effect.lua
+++ src/effect.lua
@@ -1,25 +1,3 @@
-local effects = {}
-
-local clearEffects = function()
-    -- TODO DESTROY THE TIMER
-    local elapsedTime = TimerGetElapsed(timer)
-
-    for idx, effect in pairs(effects) do
-        if (TimerGetRemaining(effect.effectTimer) <= 0.1) then
-           KillUnit(effect.unit)
-            effect.toRemove = true
-        end
-    end
-
-    local newEffects = {}
-    for idx, effect in pairs(effects) do
-        if effect.toRemove ~= true then
-            table.insert(newEffects, effect)
-        end
-    end
-    effects = newEffects
-end
-
 local createEffect = function(options)
     local x = options.x
     local y = options.y
@@ -44,20 +22,12 @@ local createEffect = function(options)
     end
 
     local effectTimer = CreateTimer()
-    TimerStart(effectTimer, options.duration, false, nil)
-    table.insert(effects, {
-        unit = dummy,
-        effectTimer = effectTimer,
-        options = options,
-    })
-end
-
-local init = function()
-    timer = CreateTimer()
-    TimerStart(timer, 0.03125, true, clearEffects)
+    TimerStart(effectTimer, options.duration, false, function()
+        KillUnit(dummy)
+        DestroyTimer(effectTimer)
+    end)
 end
 
 return {
     createEffect = createEffect,
-    init = init,
 }
