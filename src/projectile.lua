855d2ea5614f4c8bdb0e3a822ce207fd9ec9e724
unknown <kevin.m.bedi@gmail.com>
2019-08-16 20:32:49 -0700

    Add new vector lib and migrate projectiles module

--- src/projectile.lua
+++ src/projectile.lua
@@ -1,4 +1,4 @@
-local vector = require('src/vector.lua')
+local Vector = require('src/vector2.lua')
 local hero = require('src/hero.lua')
 local collision = require('src/collision.lua')
 
@@ -19,11 +19,12 @@ end
 
 local getGoalV = function(options)
     if options.toV ~= nil then
-        return options.toV
+        return Vector:new(options.toV)
     else
-        return vector.create(
-            GetUnitX(options.destUnit),
-            GetUnitY(options.destUnit))
+        return Vector:new{
+            x = GetUnitX(options.destUnit),
+            y = GetUnitY(options.destUnit),
+        }
     end
 end
 
@@ -34,7 +35,7 @@ local clearProjectiles = function()
         local curProjectileX = GetUnitX(projectile.unit)
         local curProjectileY = GetUnitY(projectile.unit)
 
-        local projectileV = vector.create(curProjectileX, curProjectileY)
+        local projectileV = Vector:new{x = curProjectileX, y = curProjectileY}
         local ownerHero = hero.getHero(projectile.options.playerId)
         local collidedUnits = collision.getAllCollisions(
             projectileV,
@@ -68,23 +69,17 @@ local clearProjectiles = function()
             destroyProjectile(projectile)
         else
             -- Move toward destination at speed
-            local totalDistX = goalV.x - curProjectileX
-            local totalDistY = goalV.y - curProjectileY
+            local distVector = Vector:new(goalV):subtract(projectileV)
 
-            local distVector = vector.create(totalDistX, totalDistY)
+            local deltaV = Vector:new(distVector)
+                :normalize()
+                :multiply(projectile.options.speed * elapsedTime)
 
-            local deltaV = vector.normalize(distVector)
-            deltaV = vector.multiply(
-                deltaV,
-                projectile.options.speed * elapsedTime)
-
-            if vector.magnitude(deltaV) >= vector.magnitude(distVector) then
+            if deltaV:magnitude() >= distVector:magnitude() then
                 deltaV = distVector
             end
 
-            deltaV = vector.add(
-                deltaV,
-                vector.create(curProjectileX, curProjectileY))
+            deltaV:add(projectileV)
 
             SetUnitX(projectile.unit, deltaV.x)
             SetUnitY(projectile.unit, deltaV.y)
@@ -107,19 +102,22 @@ end
 local createProjectile = function(options)
     local goalV = getGoalV(options)
     if options.length ~= nil then
-        local lengthNormalizedV = vector.subtract(goalV, options.fromV)
-        lengthNormalizedV = vector.normalize(lengthNormalizedV)
-        lengthNormalizedV = vector.multiply(lengthNormalizedV, options.length)
-        lengthNormalizedV = vector.add(options.fromV, lengthNormalizedV)
-        options.toV = lengthNormalizedV
+        local lengthNormalizedV = Vector:new(goalV)
+            :subtract(options.fromV)
+            :normalize()
+            :multiply(options.length)
+            :add(options.fromV)
+        goalV = lengthNormalizedV
     end
+
+    options.toV = goalV
 
     local projectile = CreateUnit(
         Player(24),
         FourCC(options.model),
         options.fromV.x,
         options.fromV.y,
-        bj_RADTODEG * Atan2(goalV.y - options.fromV.y, goalV.x - options.fromV.x))
+        bj_RADTODEG * Atan2(options.toV.y - options.fromV.y, options.toV.x - options.fromV.x))
 
     table.insert(projectiles, {
         unit = projectile,
