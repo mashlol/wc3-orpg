228cdc781c369e82a35f2dc75fa8e29fbd644ada
unknown <kevin.m.bedi@gmail.com>
2019-08-22 21:26:51 -0700

    Add a basic whirlwind

---
+++ src/spells/tarcza/whirlwind.lua
@@ -0,0 +1,91 @@
+local hero = require('src/hero.lua')
+local mouse = require('src/mouse.lua')
+local Vector = require('src/vector.lua')
+local effect = require('src/effect.lua')
+local projectile = require('src/projectile.lua')
+local collision = require('src/collision.lua')
+local log = require('src/log.lua')
+local casttime = require('src/casttime.lua')
+local animations = require('src/animations.lua')
+local damage = require('src/damage.lua')
+local cooldowns = require('src/spells/cooldowns.lua')
+
+-- TODO create some sort of helper or "DB" for getting cooldowns
+local COOLDOWN_S = 5
+
+local getSpellId = function()
+    return 'whirlwind'
+end
+
+local getSpellName = function()
+    return 'Whirlwind'
+end
+
+local cast = function(playerId)
+    if cooldowns.isOnCooldown(playerId, getSpellId()) then
+        log.log(playerId, getSpellName().." is on cooldown!", log.TYPE.ERROR)
+        return false
+    end
+
+    local hero = hero.getHero(playerId)
+
+    local heroV = Vector:new{x = GetUnitX(hero), y = GetUnitY(hero)}
+    local mouseV = Vector:new{
+        x = mouse.getMouseX(playerId),
+        y = mouse.getMouseY(playerId)
+    }
+
+    animations.queueAnimation(hero, 4, 1)
+    SetUnitFacing(hero, 0)
+
+    cooldowns.startCooldown(playerId, getSpellId(), COOLDOWN_S)
+
+    effect.createEffect{
+        model = "grns",
+        x = heroV.x,
+        y = heroV.y,
+        duration = 0.5,
+        scale = 0.5,
+        timeScale = 0.5,
+    }
+
+    local collidedUnits = collision.getAllCollisions(heroV, 100)
+    for idx, unit in pairs(collidedUnits) do
+        if IsUnitEnemy(unit, Player(playerId)) then
+            damage.dealDamage(hero, unit, 100)
+
+            effect.createEffect{
+                model = "ebld",
+                unit = unit,
+                duration = 0.1,
+            }
+        end
+    end
+
+    return true
+end
+
+local getCooldown = function(playerId)
+    return cooldowns.getRemainingCooldown(playerId, getSpellId())
+end
+
+local getTotalCooldown = function(playerId)
+    return cooldowns.getTotalCooldown(playerId, getSpellId())
+end
+
+local getIcon = function()
+    return "ReplaceableTextures\\CommandButtons\\BTNWhirlwind.blp"
+end
+
+return {
+    cast = cast,
+    getSpellId = getSpellId,
+    getSpellName = getSpellName,
+    getIcon = getIcon,
+}
+
+
+-- 4, 5, (5 ww?)
+-- 7 shield
+-- 8 shield walk
+-- 11 jab to shield?
