22932cb9682156a1d0461dfddd559e88b02410aa
unknown <kevin.m.bedi@gmail.com>
2019-08-24 22:35:16 -0700

    Fix aggro radius

--- src/camera.lua
+++ src/camera.lua
@@ -1,13 +1,13 @@
 local hero = require('src/hero.lua')
 
-function onTick()
+function onCameraTick()
     local playerId = GetPlayerId(GetLocalPlayer())
     local hero = hero.getHero(playerId)
     SetCameraQuickPosition(GetUnitX(hero), GetUnitY(hero))
 end
 
 function init()
-    TimerStart(CreateTimer(), 1, true, onTick)
+    TimerStart(CreateTimer(), 1, true, onCameraTick)
 end
 
 return {
