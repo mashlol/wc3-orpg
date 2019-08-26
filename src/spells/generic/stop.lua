c9a144779dcda32ef336429f20b17a78343efbdf
unknown <kevin.m.bedi@gmail.com>
2019-08-26 00:59:25 -0700

    Add generic spell tooltips

--- src/spells/generic/stop.lua
+++ src/spells/generic/stop.lua
@@ -12,6 +12,18 @@ local getSpellName = function()
     return 'Stop'
 end
 
+local getSpellTooltip = function(playerId)
+    return 'Press to stop your hero from moving'
+end
+
+local getSpellCooldown = function(playerId)
+    return 0
+end
+
+local getSpellCasttime = function(playerId)
+    return 0
+end
+
 local cast = function(playerId)
     return true
 end
@@ -33,4 +45,7 @@ return {
     getSpellId = getSpellId,
     getSpellName = getSpellName,
     getIcon = getIcon,
+    getSpellTooltip = getSpellTooltip,
+    getSpellCooldown = getSpellCooldown,
+    getSpellCasttime = getSpellCasttime,
 }
