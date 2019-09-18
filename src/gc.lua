b26fa4d26747d8c1b7f7b3645971d9305b6edefe
unknown <kevin.m.bedi@gmail.com>
2019-09-18 14:50:07 -0700

    Add synced gc

---
+++ src/gc.lua
@@ -0,0 +1,12 @@
+local performGC = function()
+    collectgarbage("collect")
+    collectgarbage("stop")
+end
+
+local init = function()
+    TimerStart(CreateTimer(), 30, true, performGC)
+end
+
+return {
+    init = init,
+}
