97f2a4076d006276ea0d2f425aa57045a2a930c9
unknown <kevin.m.bedi@gmail.com>
2019-09-29 16:38:27 -0700

    Fix quests, add mark above vendors

--- src/items/vendor.lua
+++ src/items/vendor.lua
@@ -3,7 +3,7 @@ local Vendor = require('src/ui/vendor.lua')
 
 local VENDORS
 
-function onNpcClicked()
+function onNpcVendorClicked()
     -- Check if its a questgiver, and if the player is close enough.
     -- If they are, give them the quest.
     -- TODO If they aren't order them to move to the questgiver and wait X time
@@ -18,21 +18,31 @@ function onNpcClicked()
         VENDORS[unitHandle] ~= nil and
         IsUnitInRange(hero, selectedUnit, 300)
     then
-        Vendor.show(playerId, {items = VENDORS[unitHandle]})
+        Vendor.show(playerId, {items = VENDORS[unitHandle].items})
     end
 end
 
 function init()
     VENDORS = {
-        [GetHandleId(gg_unit_nvlw_0015)] = {5, 6}
+        [GetHandleId(gg_unit_nvlw_0015)] = {
+            unit = gg_unit_nvlw_0015,
+            items = {5, 6}
+        }
     }
+
+    for _, vendorInfo in pairs(VENDORS) do
+        AddSpecialEffectTarget(
+            "Abilities\\Spells\\NightElf\\FaerieDragonInvis\\FaerieDragon_Invis.mdl",
+            vendorInfo.unit,
+            'overhead')
+    end
 
     local selectTrig = CreateTrigger()
     for i=0,bj_MAX_PLAYERS-1,1 do
         TriggerRegisterPlayerUnitEvent(
             selectTrig, Player(i), EVENT_PLAYER_UNIT_SELECTED, nil)
     end
-    TriggerAddAction(selectTrig, onNpcClicked)
+    TriggerAddAction(selectTrig, onNpcVendorClicked)
 end
 
 return {
