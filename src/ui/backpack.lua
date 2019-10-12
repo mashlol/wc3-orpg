de9c949fadf9f655585f452db182d4ca92d98706
unknown <kevin.m.bedi@gmail.com>
2019-10-11 18:22:29 -0700

    Enforce level requirements for items

--- src/ui/backpack.lua
+++ src/ui/backpack.lua
@@ -237,13 +237,15 @@ function Backpack:init()
                             -- Equip the item to a slot
                             local existingEquip = equipment.getItemInSlot(
                                 playerId, itemSlot)
-                            equipment.unequipItem(playerId, itemSlot)
-                            equipment.equipItem(playerId, itemSlot, itemId)
-                            backpack.removeItemFromBackpack(
-                                playerId, clickedItemPos, 1)
-                            if existingEquip ~= nil then
-                                backpack.addItemIdToBackpackPosition(
-                                    playerId, i + 1, existingEquip)
+                            local canEquip = equipment.equipItem(
+                                playerId, itemSlot, itemId)
+                            if canEquip then
+                                backpack.removeItemFromBackpack(
+                                    playerId, clickedItemPos, 1)
+                                if existingEquip ~= nil then
+                                    backpack.addItemIdToBackpackPosition(
+                                        playerId, i + 1, existingEquip)
+                                end
                             end
                         end
                     end
