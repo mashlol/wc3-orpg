145478b99fe085b129bbca87fb67e9d0aec71081
unknown <kevin.m.bedi@gmail.com>
2019-11-20 13:50:44 -0800

    Add support for editing drops in UI

--- web/src/App.js
+++ web/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import EditDialog from './EditDialog.js';
 import EditQuestDialog from './EditQuestDialog.js';
+import EditDropDialog from './EditDropDialog.js';
 
 const fs = require('fs');
 
@@ -14,11 +15,15 @@ while (depth < 20 && !fs.existsSync(path + 'map.w3x')) {
 
 const ITEMS_JSON_LOCATION = path + 'json/items.json';
 const QUESTS_JSON_LOCATION = path + 'json/quests.json';
+const DROPS_JSON_LOCATION = path + 'json/drops.json';
 
 const existingItems = JSON.parse(fs.readFileSync(ITEMS_JSON_LOCATION, {
   encoding: 'utf8'
 }));
 const existingQuests = JSON.parse(fs.readFileSync(QUESTS_JSON_LOCATION, {
+  encoding: 'utf8'
+}));
+const existingDrops = JSON.parse(fs.readFileSync(DROPS_JSON_LOCATION, {
   encoding: 'utf8'
 }));
 
@@ -33,6 +38,10 @@ class App extends React.Component {
     editQuestInfo: null,
     editQuestId: null,
     existingQuests: existingQuests,
+
+    editDropInfo: null,
+    editDropId: null,
+    existingDrops: existingDrops,
   };
 
   _onSaveItem = (info, id) => {
@@ -84,7 +93,26 @@ class App extends React.Component {
 
     // Save to filesystem
     fs.writeFileSync(QUESTS_JSON_LOCATION, JSON.stringify(oldExistingQuests, null, 2));
-  }
+  };
+
+  _onSaveDrop = (info, id) => {
+    const oldExistingDrops = this.state.existingDrops;
+    if (Object.entries(info).length > 0) {
+      oldExistingDrops[id] = info;
+    }
+    if (id !== this.state.editDropId && this.state.editDropId !== null) {
+      // We changed the ID, delete the old one
+      delete oldExistingDrops[this.state.editDropId];
+    }
+    this.setState({
+      editDropInfo: null,
+      editDropId: null,
+      existingDrops: oldExistingDrops,
+    });
+
+    // Save to filesystem
+    fs.writeFileSync(DROPS_JSON_LOCATION, JSON.stringify(oldExistingDrops, null, 2));
+  };
 
   _onCancel = (info, id) => {
     this.setState({
@@ -93,6 +121,9 @@ class App extends React.Component {
 
       editQuestInfo: null,
       editQuestId: null,
+
+      editDropInfo: null,
+      editDropId: null,
     });
   };
 
@@ -130,13 +161,31 @@ class App extends React.Component {
     });
   };
 
+  _onEditDrop = (info, id) => {
+    this.setState({
+      editDropInfo: info,
+      editDropId: id,
+    });
+    console.log('edit drop', info, id);
+  };
+
+  _onNewDrop = () => {
+    this.setState({
+      editDropInfo: {},
+      editDropId: null,
+    });
+  };
+
   render() {
     let editItemDialog = null;
     let editQuestDialog = null;
+    let editDropDialog = null;
     if (this.state.editInfo) {
       editItemDialog = <EditDialog initialData={this.state.editInfo} id={this.state.editId} onSave={this._onSaveItem} onCancel={this._onCancel} />
     } else if (this.state.editQuestInfo) {
       editQuestDialog = <EditQuestDialog initialData={this.state.editQuestInfo} id={this.state.editQuestId} onSave={this._onSaveQuest} onCancel={this._onCancel} existingItems={this.state.existingItems} existingQuests={this.state.existingQuests} />
+    } else if (this.state.editDropInfo) {
+      editDropDialog = <EditDropDialog initialData={this.state.editDropInfo} id={this.state.editDropId} onSave={this._onSaveDrop} onCancel={this._onCancel} existingItems={this.state.existingItems} existingQuests={this.state.existingQuests} />
     }
 
     const existingItemList = Object.entries(this.state.existingItems).map(itemInfo => {
@@ -148,6 +197,25 @@ class App extends React.Component {
       return <div onClick={this._onEditQuest.bind(this, questInfo[1], questInfo[0])} key={questInfo[0]}>{questInfo[1].name}</div>;
     });
     const addQuestButton = <button onClick={this._onNewQuest}>Add new quest</button>;
+
+    const existingDropTable = Object.entries(this.state.existingDrops).map(dropInfo => {
+      const dropRows = Object.entries(dropInfo[1]).map(entry => {
+        const existingItem = this.state.existingItems[entry[0]]
+        return (
+          <div className="dropTableRow">
+            <span>{existingItem ? existingItem.name : entry[0]}: </span>
+            <span>{entry[1]}</span>
+          </div>
+        );
+      });
+      return (
+        <div onClick={this._onEditDrop.bind(this, dropInfo[1], dropInfo[0])} key={dropInfo[0]}>
+          {dropInfo[0]}
+          {dropRows}
+        </div>
+      );
+    });
+    const addDropTableButton = <button onClick={this._onNewDrop}>Add new droptable</button>;
 
     let contents = null;
 
@@ -161,7 +229,7 @@ class App extends React.Component {
           {editItemDialog}
         </div>
       );
-    } else {
+    } else if (this.state.tab === 'quests') {
       contents = (
         <div>
           <div className="app">
@@ -171,6 +239,17 @@ class App extends React.Component {
           {editQuestDialog}
         </div>
       );
+    } else {
+
+      contents = (
+        <div>
+          <div className="app">
+            {existingDropTable}
+            {addDropTableButton}
+          </div>
+          {editDropDialog}
+        </div>
+      );
     }
 
     return (
@@ -178,6 +257,7 @@ class App extends React.Component {
         <div className="tabs">
           <button onClick={this._switchTab.bind(this, 'items')}>Items</button>
           <button onClick={this._switchTab.bind(this, 'quests')}>Quests</button>
+          <button onClick={this._switchTab.bind(this, 'drops')}>Drops</button>
         </div>
         {contents}
       </div>
---
+++ web/src/EditDropDialog.js
@@ -0,0 +1,118 @@
+import React from 'react';
+
+const fs = require('fs');
+
+let path = '.';
+let depth = 0;
+while (depth < 20 && !fs.existsSync(path + 'map.w3x')) {
+  path = path + '../';
+}
+
+const LUA_PATH_LOCATION = path + 'map.w3x/war3map.lua';
+
+const luaContents = fs.readFileSync(LUA_PATH_LOCATION, {encoding: 'utf8'});
+const unitFullMatch = luaContents.match(/BlzCreateUnitWithSkin\(p, FourCC\(\"([a-zA-Z0-9]{4})\"\)/g);
+const unitIds = unitFullMatch.map(x => {
+  return x.substring(33, 37);
+});
+const validUnitIds = Array.from(new Set(unitIds));
+
+class EditDropDialog extends React.Component {
+  state = {
+    data: this.props.initialData,
+    id: this.props.id,
+  };
+
+  _onSave = () => {
+    this.props.onSave(this.state.data, this.state.id);
+  };
+
+  _addNewDrop = () => {
+    const oldData = Object.assign({}, this.state.data);
+
+    let nextUnusedKey = null;
+    for (const key in this.props.existingItems) {
+      console.log('checking if key is used', key);
+      if (oldData[key] == null) {
+        nextUnusedKey = key;
+        break;
+      }
+    }
+
+    if (nextUnusedKey) {
+      oldData[nextUnusedKey] = 0;
+      this.setState({
+        data: oldData,
+      });
+    }
+  };
+
+  _onDropChange = (key, event) => {
+    const oldData = Object.assign({}, this.state.data);
+    oldData[key] = event.target.value;
+    this.setState({
+      data: oldData,
+    });
+  };
+
+  _onDropKeyChanged = (oldKey, event) => {
+    const newKey = event.target.value;
+    const oldData = Object.assign({}, this.state.data);
+    oldData[newKey] = oldData[oldKey];
+    delete oldData[oldKey];
+    this.setState({
+      data: oldData,
+    });
+  };
+
+  _onChangeUnitId = (event) => {
+    this.setState({
+      id: event.target.value,
+    });
+  };
+
+  render() {
+    const validUnitOptions = validUnitIds.map(unitId => {
+      return <option key={unitId} value={unitId}>{unitId}</option>;
+    });
+
+    const validItemOptions = Object.entries(this.props.existingItems).map(entry => {
+      const itemId = entry[0];
+      const itemName = entry[1].name;
+      return <option key={itemId} value={itemId}>{itemName}</option>
+    });
+
+    const dropRows = Object.entries(this.state.data).map(entry => {
+      return (
+        <div className="dropTableRow">
+          <select value={entry[0]} onChange={this._onDropKeyChanged.bind(this, entry[0])}>
+            <option value="none">None</option>
+            {validItemOptions}
+          </select>
+          <input type="number" value={entry[1]} onChange={this._onDropChange.bind(this, entry[0])} />
+        </div>
+      );
+    });
+
+    return (
+      <div className="editDialog">
+        <div>
+          <select value={this.state.id} onChange={this._onChangeUnitId}>
+            <option value="unset">Choose a unit id</option>
+            {validUnitOptions}
+          </select>
+        </div>
+        {dropRows}
+
+        <button onClick={this._addNewDrop}>Add new drop</button>
+
+        <hr />
+
+        <button onClick={this._onSave}>Save</button>
+        <button onClick={this.props.onCancel}>Cancel</button>
+      </div>
+    );
+  }
+}
+
+export default EditDropDialog;
