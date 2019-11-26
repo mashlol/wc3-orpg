12c43f1184c30850a26b403e3b1e549dbb0d6281
unknown <kevin.m.bedi@gmail.com>
2019-11-26 10:37:44 -0800

    Add msg when you lock/unlock

--- unlock.sh
+++ unlock.sh
@@ -3,3 +3,5 @@
 set -e
 
 git lfs unlock map.w3x/war3map.doo
+NAME=$(id -u -n)
+curl -d "content=$NAME%20has unlocked the map." https://discordapp.com/api/webhooks/648953529280495628/1u7I23u3Uc_u-82SL_ulxf15w7lf8SzwYEx_InZ8XxOOxvTTYI5rQQkqzaogF0IBKm5m
