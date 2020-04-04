c7e415b786910cc5328940522881fa728984c7f4
unknown <kevin.m.bedi@gmail.com>
2020-04-04 15:56:13 -0700

    Remove unneeded scripts

--- lock.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-git pull --rebase
-
-git lfs lock map.w3x/war3map.doo
-NAME=$(id -u -n)
-curl -d "content=$NAME%20has locked the map." https://discordapp.com/api/webhooks/648953529280495628/1u7I23u3Uc_u-82SL_ulxf15w7lf8SzwYEx_InZ8XxOOxvTTYI5rQQkqzaogF0IBKm5m
--- unlock.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-git lfs unlock map.w3x/war3map.doo
-NAME=$(id -u -n)
-curl -d "content=$NAME%20has unlocked the map." https://discordapp.com/api/webhooks/648953529280495628/1u7I23u3Uc_u-82SL_ulxf15w7lf8SzwYEx_InZ8XxOOxvTTYI5rQQkqzaogF0IBKm5m
